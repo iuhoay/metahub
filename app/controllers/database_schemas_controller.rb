6e6aeba55f262f5e2e8139a9b6a57c20b82f30fc
iuhoay <iuhoay@gmail.com>
2022-10-14 16:32:34 +0800

    Sync jobs

--- app/controllers/database_schemas_controller.rb
+++ app/controllers/database_schemas_controller.rb
@@ -2,7 +2,8 @@ class DatabaseSchemasController < ApplicationController
   before_action :set_database_schema, only: [:show, :edit, :update, :destroy, :sync_tables]
 
   def sync_tables
-    @database_schema.sync_tables
+    # @database_schema.sync_tables
+    SchemaSyncJob.perform_later(@database_schema)
     redirect_to [@database_schema.database, @database_schema], notice: 'Database schema was successfully synced.'
   end
 
---
+++ test/jobs/schema_sync_job_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class SchemaSyncJobTest < ActiveJob::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
