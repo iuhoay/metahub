3238991ba89550303daf44eb0c144aeed462d184
iuhoay <iuhoay@gmail.com>
2022-12-15 11:48:41 +0800

    Modify column type

--- app/models/table_field.rb
+++ app/models/table_field.rb
@@ -1,7 +1,7 @@
 class TableField < ApplicationRecord
   belongs_to :database_table
 
-  DATA_TYPE_REGEX = /(?<type>\w+)\(?(?<length>\d+)?,?(\d+)?/
+  DATA_TYPE_REGEX = /(?<type>\w+)\(?(?<length>\d+)?,?((?<scale>\d+))?/
 
   def to_hive_column
     "`#{field}` #{get_hive_type}"
@@ -11,14 +11,16 @@ class TableField < ApplicationRecord
     case data_type_name
     when 'varchar', 'char', 'text', 'longtext', 'mediumtext', 'tinytext', 'json'
       'string'
-    when 'int', 'tinyint', 'smallint', 'mediumint', 'bigint'
+    when 'int', 'tinyint', 'smallint'
+      'int'
+    when 'mediumint', 'bigint'
       'bigint'
-    when 'float', 'double', 'decimal'
-      'double'
+    when 'decimal'
+      "decimal(#{data_type_length}, #{data_type_scale})"
     when 'date', 'datetime', 'timestamp'
       'timestamp'
     else
-      'string'
+      raise "Unknown data type: #{data_type_name}"
     end
   end
 
@@ -30,6 +32,15 @@ class TableField < ApplicationRecord
     match = data_type.match(DATA_TYPE_REGEX)
     if match && match[:length]
       match[:length].to_i
+    else
+      nil
+    end
+  end
+
+  def data_type_scale
+    match = data_type.match(DATA_TYPE_REGEX)
+    if match && match[:scale]
+      match[:scale].to_i
     else
       nil
     end
--- test/models/table_field_test.rb
+++ test/models/table_field_test.rb
@@ -16,12 +16,18 @@ class TableFieldTest < ActiveSupport::TestCase
   test "data_type_length with no length" do
     field = TableField.new
     field.data_type = "int"
-    assert_equal nil, field.data_type_length
+    assert_nil field.data_type_length
   end
 
   test 'data_type_length with decimal' do
     field = TableField.new
     field.data_type = "decimal(10,2)"
     assert_equal 10, field.data_type_length
+  end
+
+  test 'data_type_scale with decimal' do
+    field = TableField.new
+    field.data_type = "decimal(10,2)"
+    assert_equal 2, field.data_type_scale
   end
 end
