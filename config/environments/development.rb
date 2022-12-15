3dfe9cc8b6f4bd5ad494dbb82cee9a9aab63fab4
iuhoay <iuhoay@gmail.com>
2022-12-15 09:55:11 +0800

    Add test host

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -69,4 +69,6 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 
   config.active_job.queue_adapter = :good_job
+
+  config.hosts << "meta.test"
 end
