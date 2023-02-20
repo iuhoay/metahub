4562cbc0ca21853171dc98c8a2eb14fe65edcb1e
iuhoay <iuhoay@gmail.com>
2023-02-20 16:24:16 +0800

    feat(click_house): Get connection to ClickHouse, fetch databases

--- app/models/database/click_house.rb
+++ app/models/database/click_house.rb
@@ -1,2 +1,23 @@
 class Database::ClickHouse < Database
+
+  def get_connection(database=nil)
+    ::ClickHouse::Connection.new(::ClickHouse::Config.new(
+      logger: Rails.logger,
+      host: host,
+      port: port,
+      database: database,
+      username: username,
+      password: password
+    ))
+  end
+
+  def ping
+    get_connection.ping
+  end
+
+  def sync_schemas
+    get_connection.databases.each do |database|
+      schema = schemas.find_or_create_by!(name: database)
+    end
+  end
 end
