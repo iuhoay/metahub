b247872d4e11f2ed2f9799e01e9e1e7d323aa0ae
iuhoay <iuhoay@gmail.com>
2023-02-20 16:46:56 +0800

    refactor(mysql): Move to Database::Mysql

--- app/models/database/mysql.rb
+++ app/models/database/mysql.rb
@@ -1,2 +1,15 @@
 class Database::Mysql < Database
+  def get_connection(database=nil)
+    Mysql2::Client.new(host: host, port: port, username: username, password: password, database: database)
+  end
+
+  def url
+    "mysql://#{host}:#{port}"
+  end
+
+  def sync_schemas
+    get_connection.query('SHOW DATABASES').each do |row|
+      schema = schemas.find_or_create_by!(name: row['Database'])
+    end
+  end
 end
