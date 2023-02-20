10fad8a26f3148a0a08d3a693dd62eaf9b0bb72b
iuhoay <iuhoay@gmail.com>
2023-02-20 16:40:08 +0800

    fix: Fix database_schemas url

--- app/controllers/database_schemas_controller.rb
+++ app/controllers/database_schemas_controller.rb
@@ -8,27 +8,27 @@ class DatabaseSchemasController < ApplicationController
   def sync_tables
     # @database_schema.sync_tables
     SchemaSyncJob.perform_later(@database_schema)
-    redirect_to [@database_schema.database, @database_schema], notice: 'Database schema was successfully synced.'
+    redirect_to database_database_schema_url(@database, @database_schema), notice: 'Database schema was successfully synced.'
   end
 
   def pin
     @database_schema.update(pin_at: Time.now)
-    redirect_to [@database, @database_schema], notice: 'Database table was successfully pinned.'
+    redirect_to database_database_schema_url(@database, @database_schema), notice: 'Database table was successfully pinned.'
   end
 
   def unpin
     @database_schema.update(pin_at: nil)
-    redirect_to [@database, @database_schema], notice: 'Database table was successfully unpinned.'
+    redirect_to database_database_schema_url(@database, @database_schema), notice: 'Database table was successfully unpinned.'
   end
 
   def export_hive
     CreateSqlFileJob.perform_later(@database_schema)
-    redirect_to [@database, @database_schema], notice: 'SQL files was successfully created.'
+    redirect_to database_database_schema_url(@database, @database_schema), notice: 'SQL files was successfully created.'
   end
 
   def update
     if @database_schema.update(database_schema_params)
-      redirect_to [@database, @database_schema], notice: 'Database schema was successfully updated.'
+      redirect_to database_database_schema_url(@database, @database_schema), notice: 'Database schema was successfully updated.'
     else
       render :edit
     end
