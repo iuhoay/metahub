4c5afaef832e215680f96ccedf3385dacfd62108
iuhoay <iuhoay@gmail.com>
2023-02-23 15:36:49 +0800

    chore: Change time zone `Beijing`

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,8 @@ module Metahub
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    # Time zone Asia/Shanghai
+    config.time_zone = "Beijing"
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
