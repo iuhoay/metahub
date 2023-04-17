8e4b7254994816448a90a07a1eecc116b2e70f8d
iuhoay <iuhoay@gmail.com>
2023-04-17 17:36:15 +0800

    Sentry context login user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   include Pundit::Authorization
 
   before_action :authenticate_user!, unless: :devise_controller?
+  before_action :set_sentry_context
   after_action :verify_authorized, unless: :devise_controller?
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
@@ -11,5 +12,9 @@ class ApplicationController < ActionController::Base
   def user_not_authorized
     flash[:alert] = "You are not authorized to perform this action."
     redirect_to(request.referrer || root_path)
+  end
+
+  def set_sentry_context
+    Sentry.set_user(id: current_user.id, name: current_user.name) if current_user
   end
 end
