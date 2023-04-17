e46bc56d2b332e408d8752def9e99c590b551aa8
iuhoay <iuhoay@gmail.com>
2023-04-17 16:24:11 +0800

    Improve flash message

--- app/javascript/controllers/removals_controller.js
+++ app/javascript/controllers/removals_controller.js
@@ -2,6 +2,19 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="removals"
 export default class extends Controller {
+  static targets = [ "times" ]
+
+  connect() {
+    let times = 30
+    const timer = setInterval(() => {
+      this.timesTarget.textContent = `${times -= 1}s`
+      if (times == 0) {
+        this.remove()
+        clearInterval(timer)
+      }
+    }, 1000)
+  }
+
   remove() {
     this.element.remove()
   }
