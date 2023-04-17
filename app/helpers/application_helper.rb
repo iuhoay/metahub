746863375ff9ad7a3d97210f0defa7692824460d
iuhoay <iuhoay@gmail.com>
2023-04-17 09:58:42 +0800

    User role

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,16 @@ module ApplicationHelper
       end
     end.join.html_safe
   end
+
+  def current_user_name
+    return unless user_signed_in?
+
+    if current_user.admin?
+      content_tag(:span, class: "d-flex align-items-center justify-content-center gap-1") do
+        [icon("fa-solid", "ring", class: "text-warning"), current_user.name].join.html_safe
+      end
+    else
+      content_tag(:span, current_user.name, class: "d-flex align-items-center justify-content-center gap-1")
+    end
+  end
 end
