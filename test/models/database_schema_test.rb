9864503767142f9a8b8673b047b225fd2895a96a
iuhoay <iuhoay@gmail.com>
2023-06-26 16:29:24 +0800

    Fix test

--- test/models/database_schema_test.rb
+++ test/models/database_schema_test.rb
@@ -14,10 +14,10 @@ class DatabaseSchemaTest < ActiveSupport::TestCase
   test "column_name_style" do
     database_schema = DatabaseSchema.new
     assert_equal true, database_schema.column_name_style_snake_case?
-    assert_equal false, database_schema.column_name_style_normal?
+    assert_equal false, database_schema.column_name_style_lowercase?
 
-    database_schema = DatabaseSchema.new(column_name_style: "normal")
-    assert_equal true, database_schema.column_name_style_normal?
+    database_schema = DatabaseSchema.new(column_name_style: "lowercase")
+    assert_equal true, database_schema.column_name_style_lowercase?
     assert_equal false, database_schema.column_name_style_snake_case?
   end
 end
