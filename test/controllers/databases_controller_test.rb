592c73701eeb1d516e591b5392e175e1c54711bb
iuhoay <iuhoay@gmail.com>
2023-08-08 19:28:17 +0800

    Test workflow (#12)
    * Fix user test
    * Add github test workflow

--- test/controllers/databases_controller_test.rb
+++ test/controllers/databases_controller_test.rb
@@ -1,8 +1,14 @@
 require "test_helper"
 
 class DatabasesControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    sign_in users(:admin)
+  end
+
   test "should get index" do
-    get databases_index_url
+    get databases_url
     assert_response :success
   end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,13 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get admin user" do
+    admin = users(:admin)
+    assert_not_nil admin
+  end
+
+  test "should get two users" do
+    users = User.all
+    assert_equal 2, users.size
+  end
 end
